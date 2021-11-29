f67a4a68dddf00986c16cddfeb3afa23fc4d9eff
David Diaz <david@d2weblogic.com>
2021-11-29 12:45:38 -0700

    Updates navbar.js with new brand name

--- public/navbar.js
+++ public/navbar.js
@@ -2,7 +2,7 @@ function NavBar() {
   return(
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark stroke">
       <div className="container-xl">
-        <a className="navbar-brand" href="/"><img src="./bank.png" alt="Bank logo"/> First National Bad Bank</a>
+        <a className="navbar-brand" href="/"><img src="./bank.png" alt="Bank logo"/> First National Online Bank</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
